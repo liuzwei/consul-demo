a666084689957b71daf6904bd0801157bf4a04f5
liuzhaowei <zhaowei.liu@w-oasis.com>
2018-09-19 11:48:46 +0800

    添加注释

--- consul-consumer/src/main/java/com/woasis/consulconsumer/controller/ConsumerController.java
+++ consul-consumer/src/main/java/com/woasis/consulconsumer/controller/ConsumerController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 消费者
+ */
 @RestController
 public class ConsumerController {
 
